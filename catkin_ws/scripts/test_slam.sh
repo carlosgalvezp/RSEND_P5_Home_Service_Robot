f07b99969beaf59e0476afb56f4f9e936db9d7b9
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-02-29 12:36:17 +0100

    Implement test_slam.sh script

--- catkin_ws/scripts/test_slam.sh
+++ catkin_ws/scripts/test_slam.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-xterm  -e  " gazebo " &
+set -euxo pipefail
+
+xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" &
+xterm  -e  "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm  -e  " rosrun rviz rviz"
+xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch" &
