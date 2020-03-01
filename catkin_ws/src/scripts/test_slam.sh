e25b1ae3f1b443419e67654149ca90646c3ae106
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-03-01 18:26:40 +0100

    Use own map

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 CATKIN_WS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
 export TURTLEBOT_GAZEBO_WORLD_FILE="$CATKIN_WS/src/world/carlos_big.world"
+export TURTLEBOT_GAZEBO_MAP_FILE="$CATKIN_WS/src/map/map.yaml"
 
 xterm  -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
